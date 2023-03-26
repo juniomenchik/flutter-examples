5847ee993f0422871ee29b8d242ffbceb4d6f1b5
Jeferson Junio Menchik <juniomenchik@live.com>
2023-03-26 17:28:45 -0300

    master : removendo imagem quebrada

--- dart/lib/main.dart
+++ dart/lib/main.dart
@@ -37,22 +37,13 @@ class _LoginPageState extends State<LoginPage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Container(
-                height: 150,
-                width: 150,
-                decoration: const BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage('assets/images/logo.png'),
-                  ),
-                ),
-              ),
               const SizedBox(height: 30),
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 30),
                 child: TextField(
                   controller: emailController,
                   decoration: InputDecoration(
-                    labelText: 'Email',
+                    labelText: 'Email ChongDong',
                     labelStyle: TextStyle(
                       color: Colors.grey[700],
                       fontWeight: FontWeight.bold,
